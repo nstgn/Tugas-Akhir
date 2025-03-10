61f279c94e6bdca3b8213e4d5a93b34ee5902d66
nstgn <139435736+nstgn@users.noreply.github.com>
2025-03-10 10:57:52 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,6 +15,6 @@ from streamlit_gsheets import GSheetsConnection
 #2 Input Data
 url = "https://docs.google.com/spreadsheets/d/1W9WYq245Q7g4VYn0BWt7x5DcMnhba3-rugeMu2TPM60/edit?gid=0#gid=0"
 conn = st.connection("gsheets", type=GSheetsConnection)
-data = conn.query(usecols=list(range(4)))
+data =  conn.read(spreadsheet=url, usecols=[0, 1, 2, 3])
 
 st.write("Data dari Google Sheets:", data)
