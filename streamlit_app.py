e66fda27d7e9ed4b66a34eeba7ca52c984605ccc
nstgn <139435736+nstgn@users.noreply.github.com>
2025-03-10 10:49:51 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,24 +2,19 @@
 import numpy as np
 import pandas as pd
 import tensorflow as tf
+import streamlit as st
+import plotly.graph_objects as go
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense, Dropout
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
-import matplotlib.pyplot as plt
-from google.colab import drive
-import gspread
-from google.auth import default
-from google.colab import auth
+from streamlit_gsheets import GSheetsConnection
+
 
 #2 Input Data
-auth.authenticate_user()
-creds, _ = default()
-gc = gspread.authorize(creds)
-
-sheet_url = "https://docs.google.com/spreadsheets/d/1W9WYq245Q7g4VYn0BWt7x5DcMnhba3-rugeMu2TPM60/edit?gid=0#gid=0"
-worksheet = gc.open_by_url(sheet_url).sheet1
+url = "https://docs.google.com/spreadsheets/d/1W9WYq245Q7g4VYn0BWt7x5DcMnhba3-rugeMu2TPM60/edit?gid=0#gid=0"
+conn = st.connection("gsheets", type=GSheetsConnection)
 data = pd.DataFrame(worksheet.get_all_records())
 
 
