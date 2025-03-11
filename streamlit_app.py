3d8a81ca10803d2a1ef1e85f59abcce5294cd10f
nstgn <139435736+nstgn@users.noreply.github.com>
2025-03-11 11:11:42 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -97,80 +97,9 @@ st.markdown(
     unsafe_allow_html=True,
 )
 
+# Tampilan Tabel
 elif menu == "Tabel Proteksi":
-    st.markdown(
-    """
-    <table style="width:100%; border-collapse: collapse; text-align: center;">
-        <tr>
-            <th style="border: 1px solid black; padding: 8px;">Kategori</th>
-            <th style="border: 1px solid black; padding: 8px;">Himbauan</th>
-        </tr>
-        <tr style="background-color: #00ff00;">
-            <td style="border: 1px solid black; padding: 8px; text-align: left;">0-2 (Low)</td>
-            <td style="border: 1px solid black; padding: 8px; text-align: left;">
-                <ul>
-                    <li>Tingkat bahaya rendah bagi orang banyak.</li>
-                    <li>Kenakan kacamata hitam pada hari yang cerah.</li>
-                    <li>Gunakan cairan pelembab tabir surya SPF 30+ bagi kulit sensitif.</li>
-                    <li>Permukaan yang cerah, seperti pasir, air, dan salju, akan meningkatkan paparan UV.</li>
-                </ul>
-            </td>
-        </tr>
-        <tr style="background-color: #ffff00;">
-            <td style="border: 1px solid black; padding: 8px; text-align: left;">3-5 (Moderate)</td>
-            <td style="border: 1px solid black; padding: 8px; text-align: left;">
-                <ul>
-                    <li>Tingkat bahaya sedang bagi orang yang terpapar matahari tanpa pelindung.</li>
-                    <li>Tetap di tempat teduh pada saat matahari terik siang hari.</li>
-                    <li>Kenakan pakaian pelindung matahari, topi lebar, dan kacamata hitam yang menghalangi sinar UV, pada saat berada di luar ruangan.</li>
-                    <li>Oleskan cairan pelembab tabir surya SPF 30+ setiap 2 jam bahkan pada hari berawan, setelah berenang atau berkeringat.</li>
-                    <li>Permukaan yang cerah, seperti pasir, air, dan salju, akan meningkatkan paparan UV.</li>
-                </ul>
-            </td>
-        </tr>
-        <tr style="background-color: #ff6600;">
-            <td style="border: 1px solid black; padding: 8px; text-align: left;">6-7 (High)</td>
-            <td style="border: 1px solid black; padding: 8px; text-align: left;">
-                <ul>
-                    <li>Tingkat bahaya tinggi bagi orang yang terpapar matahari tanpa pelindung, diperlukan pelindung untuk menghindari kerusakan mata dan kulit.</li>
-                    <li>Kurangi waktu di bawah paparan matahari antara pukul 10 pagi hingga pukul 4 sore.</li>
-                    <li>Kenakan pakaian pelindung matahari, topi lebar, dan kacamata hitam yang menghalangi sinar UV, pada saat berada di luar ruangan.</li>
-                    <li>Oleskan cairan pelembab tabir surya SPF 30+ setiap 2 jam bahkan pada hari berawan, setelah berenang atau berkeringat.</li>
-                    <li>Permukaan yang cerah, seperti pasir, air, dan salju, akan meningkatkan paparan UV.</li>
-                </ul>
-            </td>
-        </tr>
-        <tr style="background-color: #ff0000;">
-            <td style="border: 1px solid black; padding: 8px; text-align: left;">8-10 (Very High)</td>
-            <td style="border: 1px solid black; padding: 8px; text-align: left;">
-                <ul>
-                    <li>Tingkat bahaya tinggi bagi orang yang terpapar matahari tanpa pelindung, diperlukan pelindung untuk menghindari kerusakan mata dan kulit.</li>
-                    <li>Minimal waktu di bawah paparan matahari antara pukul 10 pagi hingga pukul 4 sore.</li>
-                    <li>Tetap di tempat teduh pada saat matahari terik siang hari.</li>
-                    <li>Kenakan pakaian pelindung matahari, topi lebar, dan kacamata hitam yang menghalangi sinar UV, pada saat berada di luar ruangan.</li>
-                    <li>Oleskan cairan pelembab tabir surya SPF 30+ setiap 2 jam bahkan pada hari berawan, setelah berenang atau berkeringat.</li>
-                    <li>Permukaan yang cerah, seperti pasir, air, dan salju, akan meningkatkan paparan UV.</li>
-                </ul>
-            </td>
-        </tr>
-        <tr style="background-color: #9900cc;">
-            <td style="border: 1px solid black; padding: 8px; text-align: left;">11+ (Extreme)</td>
-            <td style="border: 1px solid black; padding: 8px; text-align: left;">
-                <ul>
-                    <li>Tingkat bahaya ekstrem, diperlukan semua tindakan pencegahan karena kulit dan mata dapat rusak dalam hitungan menit.</li>
-                    <li>Hindari paparan matahari langsung dan pastikan perlindungan maksimal.</li>
-                    <li>Tetap di tempat teduh pada saat matahari terik siang hari.</li>
-                    <li>Kenakan pakaian pelindung matahari, topi lebar, dan kacamata hitam yang menghalangi sinar UV, pada saat berada di luar ruangan.</li>
-                    <li>Oleskan cairan pelembab tabir surya SPF 30+ setiap 2 jam bahkan pada hari berawan, setelah berenang atau berkeringat.</li>
-                    <li>Permukaan yang cerah, seperti pasir, air, dan salju, akan meningkatkan paparan UV.</li>
-                </ul>
-            </td>
-        </tr>
-    </table>
-    """,
-    unsafe_allow_html=True,
-)
-
+    
 
 elif menu == "Data Historis":
     if data is not None and not data.empty:
