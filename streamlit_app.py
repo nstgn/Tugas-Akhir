7e1176d8c5358741f0eed49f51ab0346afd34fd1
nstgn <139435736+nstgn@users.noreply.github.com>
2025-03-11 21:37:58 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,6 +63,9 @@ model = Sequential([
 #8 Pembuatan Model dan Kompilasi Model
 model.compile(optimizer='adam', loss='mean_squared_error')
 
+#9 Pelatihan Model
+history=model.fit(X_train, y_train, epochs=10, batch_size=8, validation_data=(X_test, y_test), verbose=1)
+
 # Custom Header
 st.markdown(
     """
