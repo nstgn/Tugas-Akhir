164b1451a38ba18a4c2c68a1dfcb90f272786906
nstgn <139435736+nstgn@users.noreply.github.com>
2025-03-11 21:39:34 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,6 +66,10 @@ model.compile(optimizer='adam', loss='mean_squared_error')
 #9 Pelatihan Model
 history=model.fit(X_train, y_train, epochs=10, batch_size=8, validation_data=(X_test, y_test), verbose=1)
 
+#10 Prediksi Model
+train_predicted = model.predict(X_train)
+test_predicted = model.predict(X_test)
+
 # Custom Header
 st.markdown(
     """
