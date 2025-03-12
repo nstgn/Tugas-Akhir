5797bd841f268bb34b7c004f6ba7c79a8b314643
nstgn <139435736+nstgn@users.noreply.github.com>
2025-03-12 12:07:11 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,13 @@
+#1 Import Library
 import numpy as np
 import pandas as pd
+import tensorflow as tf
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense, Dropout
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 import streamlit as st
-import matplotlib.pyplot as plt
 from streamlit_gsheets import GSheetsConnection
 import plotly.graph_objects as go
 
@@ -10,11 +16,87 @@ url = "https://docs.google.com/spreadsheets/d/1W9WYq245Q7g4VYn0BWt7x5DcMnhba3-ru
 conn = st.connection("gsheets", type=GSheetsConnection)
 data = conn.read(spreadsheet=url, usecols=[0, 1, 2, 3], ttl=0)
 
-# 2 Pre-Processing
+# 3 Pre-Processing
 if data is not None and not data.empty:
     data.columns = ["Date", "Time", "Intensity", "Index"]
     data["Waktu"] = pd.to_datetime(data["Date"] + " " + data["Time"])
     data = data.sort_values(by="Waktu")
+    data_asli = data.copy()
+    data.set_index("Waktu", inplace=True)
+    last_index = data['Index'].iloc[-1]
+    last_time = data.index[-1]
+    data = data[['Index']].copy()
+    data = data.between_time('06:00', '18:05')
+    date_range = pd.date_range(start=data.index.min(), end=data.index.max(), freq='2min')
+    data = data.reindex(date_range)
+    data['Index'].interpolate(method='linear', inplace=True)
+
+#4 Normalisasi Data
+scaler = MinMaxScaler(feature_range=(0, 1))
+data ['Index_scaled'] = scaler.fit_transform(data[['Index']])
+
+#5 Inisialisasi Timestep
+def prepare_data(series, n_steps):
+    X, y = [], []
+    for i in range(len(series)-n_steps):
+        X.append(series[i:i+n_steps])
+        y.append(series[i+n_steps])
+    return np.array(X), np.array(y)
+n_steps = 7
+X, y = prepare_data(data['Index_scaled'].values, n_steps)
+
+#6 Split Data
+split = int(len(X) * 0.8)
+X_train, X_test = X[:split], X[split:]
+y_train, y_test = y[:split], y[split:]
+X_train = X_train.reshape((X_train.shape[0], X_train.shape[1], 1))
+X_test = X_test.reshape((X_test.shape[0], X_test.shape[1], 1))
+
+#7 Bangun LSTM
+model = Sequential([
+    LSTM(50, activation='relu', input_shape=(n_steps, 1), return_sequences=True),
+    Dropout(0.2),
+    LSTM(50, activation='relu'),
+    Dense(1)
+])
+
+#8 Pembuatan Model dan Kompilasi Model
+model.compile(optimizer='adam', loss='mean_squared_error')
+
+#9 Pelatihan Model
+history=model.fit(X_train, y_train, epochs=10, batch_size=8, validation_data=(X_test, y_test), verbose=1)
+
+#10 Prediksi Model
+train_predicted = model.predict(X_train)
+test_predicted = model.predict(X_test)
+
+#11 Denormalisasi
+train_predicted_dnm = scaler.inverse_transform(train_predicted)
+test_predicted_dnm = scaler.inverse_transform(test_predicted)
+y_train_dnm = scaler.inverse_transform(y_train.reshape(-1, 1))
+y_test_dnm = scaler.inverse_transform(y_test.reshape(-1, 1))
+
+#12 Prediksi Masa Depan
+future_steps = 150  # Tentukan jumlah langkah ke depan yang ingin diprediksi
+future_input = X_test[-1].reshape(1, n_steps, 1)
+future_predictions = []
+future_timestamps = [last_time + pd.Timedelta(minutes=2 * (i + 1)) for i in range(future_steps)]
+
+for _ in range(future_steps):
+    future_pred = model.predict(future_input, verbose=0)
+    future_predictions.append(future_pred[0, 0])
+    future_input = np.roll(future_input, -1, axis=1)
+    future_input[0, -1, 0] = future_pred[0, 0]
+    
+    future_predictions_dnm = np.round (scaler.inverse_transform(np.array(future_predictions).reshape(-1, 1))).astype(int)
+    future_results = pd.DataFrame({'Datetime': future_timestamps, 'Predicted_Index': future_predictions_dnm.flatten()})
+    
+    start_time = last_time + pd.Timedelta(hours=1)
+    target_times = [start_time + pd.Timedelta(hours=i) for i in range(5)]
+    selected_rows = [
+        future_results.loc[(future_results['Datetime'] - target_time).abs().idxmin()]
+        for target_time in target_times]
+    filtered_results_1hour = pd.DataFrame(selected_rows)
 
 # Custom Header
 st.markdown(
@@ -63,11 +145,8 @@ if menu == "Beranda":
     </div>
     """, unsafe_allow_html=True)
 
-
 elif menu == "Indeks UV":
     st.subheader("🌞 Kondisi UV Sekarang")
-    last_index = data['Index'].iloc[-1]
-    last_time = data['Waktu'].iloc[-1].time()
         
     fig = go.Figure(go.Indicator(
         mode="gauge+number", value=last_index, gauge={
@@ -105,25 +184,35 @@ elif menu == "Indeks UV":
     """,unsafe_allow_html=True,)
     
     st.subheader("⏳ Prediksi Indeks UV")
-    cols = st.columns(len(future_df))
-    for i, row in future_df.iterrows():
+    cols = st.columns(len(future_results))
+    for i, row in future_results.iterrows():
         with cols[i]:
-            uv_level = row["Predicted Index"]
-            if uv_level < 3: 
-                icon, desc, bg_color = "🟢", "Low", "#00ff00"
+            uv_level = row["Predicted_Index"]
+            if uv_level < 3:
+                icon = "🟢"
+                desc = "Low"
+                bg_color = "#00ff00"
             elif uv_level < 6:
-                icon, desc, bg_color = "🟡", "Moderate", "#ffe600"
+                icon = "🟡"
+                desc = "Moderate"
+                bg_color = "#ffe600"
             elif uv_level < 8:
-                icon, desc, bg_color = "🟠", "High", "#ff8c00"
+                icon = "🟠"
+                desc = "High"
+                bg_color = "#ff8c00"
             elif uv_level < 11:
-                icon, desc, bg_color = "🔴", "Very High", "#ff0000"
+                icon = "🔴"
+                desc = "Very High"
+                bg_color = "#ff0000"
             else:
-                icon, desc, bg_color = "🟣", "Extreme", "#9900cc"
+                icon = "🟣"
+                desc = "Extreme"
+                bg_color = "#9900cc"
 
         st.markdown(
             f"""
             <div style="text-align:center; padding:10px; border-radius:5px; background-color:{bg_color};">
-                <h3 style="color:white;">{row['Time'].strftime('%H:%M')}</h3>
+                <h3 style="color:white;">{row['Datetime'].strftime('%H:%M')}</h3>
                 <h2 style="color:white;">{icon} {uv_level}</h2>
                 <p style="color:white;">{desc}</p>
             </div>
@@ -133,10 +222,6 @@ elif menu == "Indeks UV":
 
 elif menu == "Panduan Perlindungan":
     st.subheader("🛡️ Panduan Perlindungan")
-    st.markdown("""
-    <h1 style="text-align: center;margin-top: 40px; margin-bottom: 10px;">Tabel Saran Proteksi</h1>
-    """,unsafe_allow_html=True,)
-    
     st.markdown("""
     <table style="width:100%; border-collapse: collapse; text-align: center;">
         <tr>
@@ -212,8 +297,8 @@ elif menu == "Data Historis":
     if data is not None and not data.empty:
         st.subheader("📊 Data Historis Indeks UV")
         selected_columns = ["Date", "Time", "Intensity", "Index"]
-        data_filtered = data[selected_columns]
-
+        data_filtered = data_asli[selected_columns]
+        
         col1, col2 = st.columns([2, 2.5]) 
         with col1:
             st.write("📋 **Tabel Data**")
@@ -221,9 +306,9 @@ elif menu == "Data Historis":
             
         with col2:
             st.write("📈 **Grafik Indeks UV**")
-            latest_data = data.tail(100)
+            latest_data = data_asli.tail(100)
             fig = go.Figure()
-            fig.add_trace(go.Scatter(x=latest_data["Waktu"], y=latest_data["Index"],
+            fig.add_trace(go.Scatter(x=latest_data["Waktu"].dt.strftime('%H:%M'), y=latest_data["Index"],
                                  mode='lines+markers', name='Indeks',
                                  line=dict(color='#6a0dad'), fill='tozeroy'))
             fig.update_layout(
