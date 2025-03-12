5737fd059ebce058156e576d59c1fc72b294841e
nstgn <139435736+nstgn@users.noreply.github.com>
2025-03-12 09:01:37 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -181,7 +181,7 @@ elif menu == "Indeks UV":
     
     st.subheader("⏳ Prediksi Indeks UV")
     cols = st.columns(len(future_results))
-    for i, row in future_results.iterrows():
+    for i, (index, row) in enumerate(future_results.iterrows()):
         with cols[i]:
             uv_level = row["Predicted_Index"]
             if uv_level < 3:
